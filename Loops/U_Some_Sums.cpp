23ced6a29c3ce7ea92f87b8502ecf3e49abeb425
Rajarshi Roy <roystark.dev@gmail.com>
2023-08-29 15:07:27 +0530

    Solved R, T, U, W, X

--- Loops/U_Some_Sums.cpp
+++ Loops/U_Some_Sums.cpp
@@ -1,13 +1,27 @@
+//loops
 #include<bits/stdc++.h>
 using namespace std;
 #define endl '\n'
-int main() {
-    int n, a, b; cin >> n >> a >> b;
-    for(int i = 2; i <= n; i++) {
-        for(int j = 2; j <= b; j++) {
-            sum += j % 10;
-            
-        }
+
+bool sumCheck(int i, int a, int b) {//2 2 5
+    int sum = 0;
+    int rem;
+    while(i > 0) {
+        rem = i % 10;// 2
+        sum += rem;//2
+        i /= 10;//0
     }
+    if(sum >= a && sum <= b) return true;
+    else return false;
+}
+
+int main() {
+    int n, a, b; cin >> n >> a >> b;//20 2 5
+    int sum = 0;
+    for(int i = 1; i <= n; i++) {//2
+        if(sumCheck(i, a, b)) sum += i;
+    }
+    cout << sum;
     return 0;
 }
+
